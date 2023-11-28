66fbce7373b6ba524a1870bf68824b2d2a5b1bb5
C00zzy <152229944+C00zzy@users.noreply.github.com>
2023-11-27 19:47:57 -0500

    Update changesource2sid.sh

--- changesource2sid.sh
+++ changesource2sid.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 #This for debian 12, Any release testing or stable. Sid is unstable though i wouldn't use it.
 #Use openSUSE or arch if you want new packages to be honest.
-echo "Updating and upgrading the system first"
-  sudo apt update && sudo apt upgrade && apt update && apt upgrade
-echo "Removing old repos"
+echo "Updating and upgrading the system first also removing old repos..."
+sudo apt update && sudo apt upgrade && apt update && apt upgrade
 rm /etc/apt/sources.list
 echo "setting new repos"
 echo "deb http://deb.debian.org/debian/ unstable main contrib non-free" >> /etc/apt/sources.list
