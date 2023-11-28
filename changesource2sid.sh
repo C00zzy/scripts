ab98025ce26e20ca5bad034852693de394ccfcdd
C00zzy <152229944+C00zzy@users.noreply.github.com>
2023-11-27 19:53:25 -0500

    Update changesource2sid.sh

--- changesource2sid.sh
+++ changesource2sid.sh
@@ -1,15 +1,23 @@
-# /bin/bash
-#This for debian 12, Any release testing or stable. Sid is unstable though i wouldn't use it.
-#Use openSUSE or arch if you want new packages to be honest.
-echo "Updating and upgrading the system first also removing old repos..."
-sudo apt update && sudo apt upgrade && apt update && apt upgrade
-rm /etc/apt/sources.list
-echo "setting unstable repos..."
-echo "deb http://deb.debian.org/debian/ unstable main contrib non-free" >> /etc/apt/sources.list
-echo "deb-src http://deb.debian.org/debian/ unstable main contrib non-free" >> /etc/apt/sources.list
-"Echo upgrading system"
-apt update && apt upgrade && sudo apt update && sudo apt upgrade
-echo "Don't worry about 
-echo "Have fun, Don't break it!"
-echo "Make sure it's right."
-cat /etc/apt/sources.list
+#!/bin/bash
+
+# Prompt the user with a yes/no question
+read -p "Do you want to set up sid repos? (y/n): " answer
+
+# Check the user's response
+if [[ "$answer" == [Yy]* ]]; then
+    echo "Updating and upgrading the system first, also removing old repos..."
+    sudo apt update && sudo apt upgrade && apt update && apt upgrade
+    rm /etc/apt/sources.list
+
+    echo "Setting up unstable repos..."
+    echo "deb http://deb.debian.org/debian/ unstable main contrib non-free" | sudo tee -a /etc/apt/sources.list >/dev/null
+    echo "deb-src http://deb.debian.org/debian/ unstable main contrib non-free" | sudo tee -a /etc/apt/sources.list >/dev/null
+
+    echo "Upgrading system..."
+    apt update && apt upgrade && sudo apt update && sudo apt upgrade
+
+    echo "Have fun, but be cautious! Make sure it's right."
+    cat /etc/apt/sources.list
+else
+    echo "Skipping the setup of unstable repositories. Exiting."
+fi
