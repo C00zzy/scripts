71b49ac00ff8c706e14e93812eb6a9682154cb0d
Or <152229944+C00zzy@users.noreply.github.com>
2023-11-27 21:10:06 -0500

    Update pacmanaliases.sh

--- pacmanaliases.sh
+++ pacmanaliases.sh
@@ -3,22 +3,13 @@
 #Linking sudo and doas incase its using doas
 #Also this installs paru! That's cool.
 ln -s $(which doas) /usr/bin/sudo
-echo "This is for bash btw"
-echo "Installing sudo incase you don't have it"
+sudo pacman -S doas
 echo "alias pacupdate='sudo pacman -Syu'" >> .bashrc
 echo "alias pacinstall='sudo pcman -Syu'" >> .bashrc
 echo "alias pacremove='sudo pcman -Rs'" >> .bashrc
 echo "alias pacautoremove='pacman -Rdd'" >> .bashrc
 echo "alias pacsearch='pacman -Qe'" >> .bashrc
-#Alright This is for paru
 echo "Installing paru incase it doesn't have it"
 echo "alias auradd='paru -S'" >> .bashrc
 echo "alias aurremove='paru -Rs'" >> .bashrc
 echo "alias aursearch='paru -Ss'" >> .bashrc
-sudo pacman -S git cargo base-devel
-git clone https://aur.archlinux.org/paru-git
-cd paru-git
-makepkg
-echo "Do makepkg, Then sudo pacman -U"
-#END
-clear
