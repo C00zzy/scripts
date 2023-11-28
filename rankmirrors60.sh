f8816de70dbabbc7694abfda1f40d7b5228b5977
C00zzy <152229944+C00zzy@users.noreply.github.com>
2023-11-27 19:07:01 -0500

    Create rankmirrors60.sh

---
+++ rankmirrors60.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+#This ranks mirrors for pacman.
+#Needs reflector.
+#Run this as root or it probably won't work.
+echo "installing Reflector.."
+sudo pacman -S --noconfirm reflector
+clear
+echo "Removing old mirrorlist and making a backup of it"
+sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+sudo rm /etc/pacman.d/mirrorlist
+clear
+echo "Ranking mirrors.."
+sudo reflector --latest 60 -protocol https --sort rate --save /etc/pacman.d/mirrorlist && echo done
