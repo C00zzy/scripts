46007712f38ff32a4914b67771246e56d477f2ee
Or <152229944+C00zzy@users.noreply.github.com>
2023-12-03 14:05:57 -0500

    Update secureboot.sh

--- secureboot.sh
+++ secureboot.sh
@@ -1,8 +1,9 @@
 #! /bin/sh
 echo "installing sbctl"
 pacman -S --noconfirm sbctl
+sbctl create-keys
+sbctl enroll-keys -m -f
 sbctl verify
 sbctl sign /boot/vmlinuz-linux-zen
 sbctl sign /boot/EFI/BOOT/BOOTX64.EFI
 sbctl sign /boot/EFI/systemd/systemd-bootx64.efi
-
