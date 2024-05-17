839f61c547482c4140242a537d75611c01ff0879
Or <152229944+C00zzy@users.noreply.github.com>
2024-05-17 13:05:40 -0400

    newcript

---
+++ bash-scripts/fix-xdg.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+echo "Only works for sway btw"
+systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
